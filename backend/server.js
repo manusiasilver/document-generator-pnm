97465f784805b9fbd87b74e025afa1003d04824e
apri0429 <apriganteng91@gmail.com>
2026-05-05 09:56:35 +0700

    port

--- backend/server.js
+++ backend/server.js
@@ -228,7 +228,7 @@ if (fs.existsSync(distPath)) {
     });
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8096;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
